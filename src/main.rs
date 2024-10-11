12b7a47355c09e52351e970a13cac3b739c6408d
Luis Demiurgos <luisdemiurgos@gmail.com>
2024-10-11 08:15:02 -0500

    [feat]: Add function to setup de database connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-use std::{error, result};
+use std::{env, error, fs, io, path::PathBuf, result};
+
+use rusqlite::Connection;
 
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
-fn main() {
-    println!("Hello, world!");
+const DEFAULT_PATH: &str = "./database/axelrod.db";
+
+fn setup() -> Result<Connection> {
+    let env_var = env::var("DATABASE_PATH").unwrap_or_else(|_|{
+        println!("Warning: The `DATABASE_PATH` environment variable is not set, {DEFAULT_PATH} will be used instead.");
+        DEFAULT_PATH.into()
+    });
+    let db_path = PathBuf::from(env_var);
+    let db_dir = &db_path.parent().ok_or(io::Error::new(
+        io::ErrorKind::InvalidInput,
+        "Error: Invalid path to database",
+    ))?;
+    if !&db_path.exists() {
+        fs::create_dir_all(db_dir)?;
+        println!(
+            "Warning: database at {} does not exists, will be created.",
+            db_path.display()
+        );
+    }
+    let conn = Connection::open(&db_path)?;
+    Ok(conn)
+}
+
+fn main() -> Result<()> {
+    setup()?;
+    Ok(())
 }
